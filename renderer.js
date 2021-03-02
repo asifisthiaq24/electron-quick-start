de2e49f2549f1e1e38b714a55fed1435dd2e2d5b
asifisthiaq24 <asifisthiaq24@gmail.com>
2021-03-03 05:31:41 +0600

    Update renderer

--- renderer.js
+++ renderer.js
@@ -18,11 +18,13 @@ const notification = document.getElementById('notification');
 const message = document.getElementById('message');
 const restartButton = document.getElementById('restart-button');
 ipcRenderer.on('update_available', () => {
+    console.log("updates available")
     ipcRenderer.removeAllListeners('update_available');
     message.innerText = 'A new update is available. Downloading now...';
     notification.classList.remove('hidden');
 });
 ipcRenderer.on('update_downloaded', () => {
+    console.log("updates downloaded")
     ipcRenderer.removeAllListeners('update_downloaded');
     message.innerText = 'Update Downloaded. It will be installed on restart. Restart now?';
     restartButton.classList.remove('hidden');
