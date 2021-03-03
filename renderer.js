f39d3cda4413bac4898d0de6f4d6c00ff5ec0799
Asif Isthiaq <isthiaq@tigerit.com>
2021-03-03 10:22:35 +0600

    Initial commit for the purpose of testing autoupdating

---
+++ renderer.js
@@ -0,0 +1,37 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off. Use `preload.js` to
+// selectively enable features needed in the rendering
+// process.
+const { ipcRenderer } = require("electron");
+const version = document.getElementById("version");
+
+ipcRenderer.send("app_version");
+ipcRenderer.on("app_version", (event, arg) => {
+    console.log(arg);
+    ipcRenderer.removeAllListeners("app_version");
+    version.innerText = "Version " + arg.version;
+});
+
+const notification = document.getElementById('notification');
+const message = document.getElementById('message');
+const restartButton = document.getElementById('restart-button');
+ipcRenderer.on('update_available', () => {
+    ipcRenderer.removeAllListeners('update_available');
+    message.innerText = 'A new update is available. Downloading now...';
+    notification.classList.remove('hidden');
+});
+ipcRenderer.on('update_downloaded', () => {
+    ipcRenderer.removeAllListeners('update_downloaded');
+    message.innerText = 'Update Downloaded. It will be installed on restart. Restart now?';
+    restartButton.classList.remove('hidden');
+    notification.classList.remove('hidden');
+});
+
+function closeNotification() {
+    notification.classList.add('hidden');
+}
+function restartApp() {
+    ipcRenderer.send('restart_app');
+}
